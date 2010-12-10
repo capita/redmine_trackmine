cff78cbbeab436938f4ee498e81367e2a8d657e5
Piotr Brudny <piotrek@therock.pl>
2010-12-10 15:10:48 +0100

    Added edit, update to Mapping controller

--- app/controllers/mappings_controller.rb
+++ app/controllers/mappings_controller.rb
@@ -13,21 +13,38 @@ class MappingsController < ApplicationController
                                   
     @projects = Project.all
     @tracker_projects = Trackmine.projects
-    @labels = ['..choose..']
+    @labels = [['..choose..','']]
+  end
+  
+  def edit
+    @mapping = Mapping.find params[:id]   
   end
 
   def create
-    @mapping = Mapping.new params[:mapping].merge :estimations => { 1 => 1, 2 => 4, 3 => 10 },
-                                                  :story_types => { 'feature' => 'Feature', 'bug' => 'Bug', 'chore' => 'Support' }
+    @mapping = Mapping.new params[:mapping].merge :estimations => params[:estimations],
+                                                  :story_types => params[:story_types]
     
     @mapping.tracker_project_id = params[:tracker_project_id]
     @mapping.tracker_project_name = PivotalTracker::Project.find(params[:tracker_project_id].to_i).name 
     if @mapping.save
       flash[:notice] = 'Mapping was successfully added.'
-      redirect_to :action => "index", :project_id => @project    
+      redirect_to :action => "index"
     else
       flash[:error] = "Can't map these projects."
-      redirect_to :action => "new", :project_id => @project    
+      redirect_to :action => "new"
+    end
+  end
+  
+  def update
+    @mapping = Mapping.find params[:id]
+    if @mapping.update_attributes :estimations => params[:estimations],
+                                  :story_types => params[:story_types]
+
+      flash[:notice] = 'Updated successfully.'
+      redirect_to :action => "index"
+    else
+      flash[:error] = "Can't save that configuration."
+      redirect_to :action => "new"
     end
   end
 
