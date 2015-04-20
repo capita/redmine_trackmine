4b1f20fe5714f115288a9f10aaed4ab8325254c1
Piotr Brudny <pbrudny@gmail.com>
2015-04-20 18:49:30 +0200

    Fixed loading PivotalHandler

--- init.rb
+++ init.rb
@@ -1,23 +1,17 @@
 require 'redmine'
-# require 'dispatcher'
-# require 'bundler'
-Bundler.setup(:default)
 require 'pivotal_tracker'
 require 'unicode'
 
-# RAILS_DEFAULT_LOGGER.info 'Starting Trackmine Plugin for Redmine'
+Rails.logger.info 'Starting Trackmine Plugin for Redmine'
 
-# Patches to the Redmine core.
 require_dependency 'project_patch'
 require_dependency 'issue_patch'
 
-# Dispatcher.to_prepare do
-#   config.middleware.insert_after ActionController::StringCoercion, "PivotalHandler"
-  Issue.send(:include, IssuePatch)
-  Project.send(:include, ProjectPatch)
-# end
+Rails.application.config.middleware.insert_before(Rack::Runtime, 'PivotalHandler' )
 
-# Sets error email for trackmine mailer
+Issue.send(:include, IssuePatch)
+Project.send(:include, ProjectPatch)
+
 Trackmine.set_error_notification
 
 Redmine::Plugin.register :redmine_trackmine do
@@ -26,5 +20,5 @@ Redmine::Plugin.register :redmine_trackmine do
   description 'This plugin integrates Redmine projects with Pivotal Tracker'
   version '2.0.1'
 
-  menu :admin_menu, :mapping, { :controller => :mappings, :action => 'index' }, :caption =>'Trackmine', :last => true
+  menu :admin_menu, :mapping, {controller: :mappings, action: 'index'}, caption: 'Trackmine', last: true
 end
